2ea176cdd5ac0e78fc84da653968f31be360d8d4
Christopher Boyle <cboyle824@gmail.com>
2021-08-09 22:59:31 -0400

    completed Movie reducer section

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
-import { Provider} from 'react-redux';
+import "./index.css";
 
-import reducer from './reducers';
+import App from "./App";
+import { Provider } from "react-redux";
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import { createStore } from "redux";
+import reducer from "./reducers";
 
-import App from './App'
-import { BrowserRouter as Router } from 'react-router-dom';
-import './index.css';
+const store = createStore(reducer);
 
 ReactDOM.render(
   <Router>
+    <Provider store={store}>
       <App />
+    </Provider>
   </Router>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
