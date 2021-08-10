f1c8535a2453930a5457c369a4667c4a786786ab
Christopher Boyle <cboyle824@gmail.com>
2021-08-09 23:59:15 -0400

    adding and deleting movies now work

--- src/reducers/movieReducer.js
+++ src/reducers/movieReducer.js
@@ -13,6 +13,12 @@ const reducer = (state = initialState, action) => {
       return {
         movies: state.movies.filter((item) => action.payload !== item.id),
       };
+    case ADD_MOVIE:
+      return {
+        ...state,
+        movies: [...state.movies, action.payload],
+      };
+
     default:
       return state;
   }
